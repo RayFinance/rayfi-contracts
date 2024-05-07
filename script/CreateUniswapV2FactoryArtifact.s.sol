f2e3e652de8ccf933816979a8b9e0857999061f0
0xC4LL3 <calleandrea_@outlook.com>
2024-05-07 14:48:55 +0200

    added scripts for the creation of uniswap v2 artifcats

---
+++ script/CreateUniswapV2FactoryArtifact.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.5.16;
+
+import {UniswapV2Factory} from "@uniswap/v2-core/contracts/UniswapV2Factory.sol";
+
+contract DeployUniswapV2Mocks {}
---
+++ script/CreateUniswapV2RouterArtifact.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.6;
+
+import {WETH9} from "@uniswap/v2-periphery/contracts/test/WETH9.sol";
+import {UniswapV2Router02} from "@uniswap/v2-periphery/contracts/UniswapV2Router02.sol";
+
+contract CreateUniswapV2RouterArtifact {}
