fac8c00b72c5a12deca316c0c786cf60447cdd18
0xC4LL3 <calleandrea_@outlook.com>
2024-05-22 15:17:43 +0200

    added fuzz tests for preservation of protocol properties

--- test/fuzz/RayFiFuzz.t.sol
+++ test/fuzz/RayFiFuzz.t.sol
@@ -28,6 +28,120 @@ contract Fuzz is Test {
         vm.stopPrank();
     }
 
+    function testFuzz_sumsArePreservedOnTransfers(address to, uint256 value) public {
+        if (to == address(0)) {
+            return;
+        }
+
+        value = bound(value, 0, rayFi.balanceOf(msg.sender));
+        uint256 fromBalance = rayFi.balanceOf(msg.sender);
+        uint256 toBalance = rayFi.balanceOf(to);
+        uint256 totalSupply = rayFi.totalSupply();
+        uint256 totalRewardShares = rayFi.getTotalRewardShares();
+        uint256 fromBalanceAtSnapshot = rayFi.getBalanceOfAtSnapshot(msg.sender, 0);
+        uint256 toBalanceAtSnapshot = rayFi.getBalanceOfAtSnapshot(to, 0);
+        uint256 fromSharesBalance = rayFi.getSharesBalanceOf(msg.sender);
+        uint256 toSharesBalance = rayFi.getSharesBalanceOf(to);
+        uint256 totalRewardSharesAtSnapshot = rayFi.getTotalRewardSharesAtSnapshot(0);
+
+        vm.prank(msg.sender);
+        rayFi.transfer(to, value);
+
+        uint256 valueReceived = value - value * (BUY_FEE + SELL_FEE) / 100;
+        assertEq(rayFi.balanceOf(msg.sender), fromBalance - value);
+        assertEq(rayFi.balanceOf(to), toBalance + valueReceived);
+        assert(rayFi.totalSupply() >= totalSupply - ACCEPTED_PRECISION_LOSS);
+        assert(rayFi.totalSupply() <= totalSupply);
+        assertEq(rayFi.getCurrentSnapshotId(), 0);
+        if (
+            rayFi.balanceOf(msg.sender) >= MINIMUM_TOKEN_BALANCE_FOR_REWARDS
+                && rayFi.balanceOf(to) >= MINIMUM_TOKEN_BALANCE_FOR_REWARDS
+        ) {
+            assertEq(rayFi.getBalanceOfAtSnapshot(msg.sender, 0), fromBalanceAtSnapshot - value);
+            assertEq(rayFi.getSharesBalanceOf(msg.sender), fromSharesBalance - value);
+            assertEq(rayFi.getBalanceOfAtSnapshot(to, 0), toBalanceAtSnapshot + valueReceived);
+            assertEq(rayFi.getSharesBalanceOf(to), toSharesBalance + valueReceived);
+            assert(
+                rayFi.getTotalRewardShares()
+                    >= totalRewardShares - value * (BUY_FEE + SELL_FEE) / 100 - ACCEPTED_PRECISION_LOSS
+            );
+            assert(rayFi.getTotalRewardShares() <= totalRewardShares - value * (BUY_FEE + SELL_FEE) / 100);
+            assert(
+                rayFi.getTotalRewardSharesAtSnapshot(0)
+                    >= totalRewardSharesAtSnapshot - value * (BUY_FEE + SELL_FEE) / 100 - ACCEPTED_PRECISION_LOSS
+            );
+            assert(
+                rayFi.getTotalRewardSharesAtSnapshot(0)
+                    <= totalRewardSharesAtSnapshot - value * (BUY_FEE + SELL_FEE) / 100
+            );
+        } else if (rayFi.balanceOf(to) >= MINIMUM_TOKEN_BALANCE_FOR_REWARDS) {
+            assertEq(rayFi.getBalanceOfAtSnapshot(msg.sender, 0), 0);
+            assertEq(rayFi.getSharesBalanceOf(msg.sender), 0);
+            assertEq(rayFi.getBalanceOfAtSnapshot(to, 0), toBalanceAtSnapshot + valueReceived);
+            assertEq(rayFi.getSharesBalanceOf(to), toSharesBalance + valueReceived);
+            assertEq(rayFi.getTotalRewardShares(), rayFi.balanceOf(to));
+            assertEq(rayFi.getTotalRewardSharesAtSnapshot(0), rayFi.balanceOf(to));
+        } else {
+            assertEq(rayFi.getBalanceOfAtSnapshot(msg.sender, 0), fromBalanceAtSnapshot - value);
+            assertEq(rayFi.getSharesBalanceOf(msg.sender), fromSharesBalance - value);
+            assertEq(rayFi.getBalanceOfAtSnapshot(to, 0), 0);
+            assertEq(rayFi.getSharesBalanceOf(to), 0);
+            assertEq(rayFi.getTotalRewardShares(), rayFi.balanceOf(msg.sender));
+            assertEq(rayFi.getTotalRewardSharesAtSnapshot(0), rayFi.balanceOf(msg.sender));
+        }
+    }
+
+    function testFuzz_sumsArePreservedOnStaking(uint256 value) public {
+        address to = rayFi.getVaultTokens()[0];
+        value = bound(value, 0, rayFi.balanceOf(msg.sender));
+        uint256 fromBalance = rayFi.balanceOf(msg.sender);
+        uint256 totalSupply = rayFi.totalSupply();
+        uint256 totalRewardShares = rayFi.getTotalRewardShares();
+        uint256 fromBalanceAtSnapshot = rayFi.getBalanceOfAtSnapshot(msg.sender, 0);
+        uint256 fromSharesBalance = rayFi.getSharesBalanceOf(msg.sender);
+        uint256 totalRewardSharesAtSnapshot = rayFi.getTotalRewardSharesAtSnapshot(0);
+
+        vm.prank(msg.sender);
+        rayFi.stake(to, value);
+
+        assertEq(rayFi.balanceOf(msg.sender), fromBalance - value);
+        assertEq(rayFi.balanceOf(to), value);
+        assertEq(rayFi.totalSupply(), totalSupply);
+        assertEq(rayFi.getTotalRewardShares(), totalRewardShares);
+        assertEq(rayFi.getTotalStakedShares(), value);
+        assertEq(rayFi.getCurrentSnapshotId(), 0);
+        assertEq(rayFi.getBalanceOfAtSnapshot(msg.sender, 0), fromBalanceAtSnapshot - value);
+        assertEq(rayFi.getSharesBalanceOf(msg.sender), fromSharesBalance);
+        assertEq(rayFi.getBalanceOfAtSnapshot(to, 0), 0);
+        assertEq(rayFi.getSharesBalanceOf(to), 0);
+        assertEq(rayFi.getTotalRewardShares(), totalRewardShares);
+        assertEq(rayFi.getTotalRewardSharesAtSnapshot(0), totalRewardSharesAtSnapshot);
+        assertEq(rayFi.getTotalStakedShares(), value);
+        assertEq(rayFi.getTotalStakedSharesAtSnapshot(0), value);
+        assertEq(rayFi.getVaultBalanceOf(to, msg.sender), value);
+        assertEq(rayFi.getTotalVaultShares(to), value);
+
+        vm.prank(msg.sender);
+        rayFi.unstake(to, value);
+
+        assertEq(rayFi.balanceOf(msg.sender), fromBalance);
+        assertEq(rayFi.balanceOf(to), 0);
+        assertEq(rayFi.totalSupply(), totalSupply);
+        assertEq(rayFi.getTotalRewardShares(), totalRewardShares);
+        assertEq(rayFi.getTotalStakedShares(), 0);
+        assertEq(rayFi.getCurrentSnapshotId(), 0);
+        assertEq(rayFi.getBalanceOfAtSnapshot(msg.sender, 0), fromBalanceAtSnapshot);
+        assertEq(rayFi.getSharesBalanceOf(msg.sender), fromSharesBalance);
+        assertEq(rayFi.getBalanceOfAtSnapshot(to, 0), 0);
+        assertEq(rayFi.getSharesBalanceOf(to), 0);
+        assertEq(rayFi.getTotalRewardShares(), totalRewardShares);
+        assertEq(rayFi.getTotalRewardSharesAtSnapshot(0), totalRewardSharesAtSnapshot);
+        assertEq(rayFi.getTotalStakedShares(), 0);
+        assertEq(rayFi.getTotalStakedSharesAtSnapshot(0), 0);
+        assertEq(rayFi.getVaultBalanceOf(to, msg.sender), 0);
+        assertEq(rayFi.getTotalVaultShares(to), 0);
+    }
+
     function testFuzz_cannotTransferMoreThanOwned(uint256 value) public {
         if (value == 0) {
             return;
